ba61d7a0b98a6fa640e0add16edd5418bd809048
Daniel Getz <tanketz@gmail.com>
2014-11-26 06:25:47 -0100

    JsonObjectInputter: fix initial freeze and nulls at end of input  - create JsonParser on first call to readOneValue()  - check result of readValueAsTree() for null or non-object JSON

--- src/main/java/djgcv/ssjp/util/flow/jackson/JsonObjectInputter.java
+++ src/main/java/djgcv/ssjp/util/flow/jackson/JsonObjectInputter.java
@@ -1,31 +1,46 @@
 package djgcv.ssjp.util.flow.jackson;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import djgcv.ssjp.util.flow.io.Inputter;
 
 public class JsonObjectInputter extends Inputter<ObjectNode> {
-  private final JsonParser parser;
+  private final JsonFactory factory;
+  private JsonParser parser;
 
   public JsonObjectInputter(JsonFactory factory, InputStream inputStream,
-      boolean manageInputStream) throws IOException {
+      boolean manageInputStream) {
     super(inputStream, manageInputStream);
-    parser = factory.createParser(getInputStream());
+    this.factory = factory;
   }
 
   @Override
   protected ObjectNode readOneValue() throws IOException {
-    return parser.readValueAsTree();
+    if (parser == null) {
+      parser = factory.createParser(getInputStream());
+    }
+    JsonNode value = parser.readValueAsTree();
+    if (value == null) {
+      throw new EOFException();
+    } else if (value.isObject()) {
+      return (ObjectNode) value;
+    } else {
+      throw new IOException("Wrong type of JSON data: " + value.getNodeType());
+    }
   }
 
   @Override
   protected void performClose() {
     super.performClose();
-    closeQuietly(parser);
+    if (parser != null) {
+      closeQuietly(parser);
+    }
   }
 }
