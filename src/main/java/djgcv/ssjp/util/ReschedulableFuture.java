fd2f2303cb923356af2dafae9c139b450e9b32e0
Daniel Getz <tanketz@gmail.com>
2014-11-29 14:01:17 -0100

    remove unused Rescheduler

--- src/main/java/djgcv/ssjp/util/ReschedulableFuture.java
+++
@@ -1,9 +0,0 @@
-package djgcv.ssjp.util;
-
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.util.concurrent.ListenableScheduledFuture;
-
-public interface ReschedulableFuture<V> extends ListenableScheduledFuture<V> {
-  boolean reschedule(long delay, TimeUnit unit);
-}
